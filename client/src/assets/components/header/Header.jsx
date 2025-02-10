bf7d6282b3dc095db6a8f1a3e32a7e6f10c06479
Christopher Totlyakov <laminex0622@gmail.com>
2025-02-10 21:40:10 +0200

    headerBar design

--- client/src/assets/components/header/Header.jsx
+++ client/src/assets/components/header/Header.jsx
@@ -1,14 +1,15 @@
-
 import { Navigation } from "../Navigation";
+import "./Header.css";
 
-
-export function Header(){
-    return(
-        <>
-        <header>
-            <h1>Shop</h1>
-            <Navigation/>
-        </header>
-        </>
-    )
+export function Header() {
+  return (
+    <header className="custom-header">
+      <div className="header-left">
+        <h1>Shop</h1>
+      </div>
+      <div className="header-right">
+        <Navigation />
+      </div>
+    </header>
+  );
 }
