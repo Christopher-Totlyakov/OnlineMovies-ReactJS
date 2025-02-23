b852520410f6dae1a93d6f903edcbf2b145cf359
Christopher Totlyakov <laminex0622@gmail.com>
2025-02-24 01:08:25 +0200

    in movie details add imgBackdrop styles

--- client/src/assets/components/movies/MovieDetails.jsx
+++ client/src/assets/components/movies/MovieDetails.jsx
@@ -2,13 +2,15 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { movieDetailsByID } from "../../../api/dataMovies"
 
+import style from "./MovieDetails.module.css"
+
 export default function MovieDetails() {
     const [movieDetails, setMovieDetails] = useState({});
     const { id } = useParams();
 
     useEffect(() => {
         const controller = new AbortController();
-    
+
         (async () => {
             try {
                 const response = await movieDetailsByID(controller.signal, id);
@@ -20,12 +22,33 @@ export default function MovieDetails() {
                 }
             }
         })();
-    
+
         return () => controller.abort();
-    }, [id]); 
-    
+    }, [id]);
+
 
     return (
-        <h1>{movieDetails.title}</h1>
+        <>
+            <div className={style['imgBackdropConteiner']} >
+                <div
+                    className={style['imgBackdrop']}
+                    style={{ '--backdrop-img': `url(https://image.tmdb.org/t/p/w1920${movieDetails.backdrop_path})`}}>
+
+                </div>
+            </div>
+            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails.title}</h1>
+            <h1>{movieDetails.title}</h1>
+        </>
+
     );
 }
