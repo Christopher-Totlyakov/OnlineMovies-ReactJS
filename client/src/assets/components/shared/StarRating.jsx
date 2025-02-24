cfb405b0df0a024ed052bdff8e4154d7fc751e94
Christopher Totlyakov <laminex0622@gmail.com>
2025-02-25 00:50:22 +0200

    rating stars

---
+++ client/src/assets/components/shared/StarRating.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './StarRating.module.css'; // CSS файлът със стиловете
+
+const StarRating = ({ rating, maxStars = 10 }) => {
+    const percentage = (rating / maxStars) * 100;
+
+    return (
+        <div className={styles.starContainer}>
+            <div className={styles.stars} style={{ width: `${percentage}%` }}>
+                {Array.from({ length: maxStars }, (_, index) => (
+                    <span key={index}>★</span>
+                ))}
+            </div>
+            <div className={styles.grayStars}>
+                {Array.from({ length: maxStars }, (_, index) => (
+                    <span key={index}>★</span>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default StarRating;
