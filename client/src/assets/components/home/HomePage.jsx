da42eb42956a9b43b0e95e9649e1787d108a3064
Christopher Totlyakov <laminex0622@gmail.com>
2025-03-20 17:10:33 +0200

    home page style

--- client/src/assets/components/home/HomePage.jsx
+++ client/src/assets/components/home/HomePage.jsx
@@ -1,17 +1,19 @@
 import { useLoadTrending } from "../../hooks/useLoadTrending";
 import { Slider } from "./Slider";
+import "./HomePage.css"
 
 export function HomePage() {
     const { details: movieDetails, loading: movieLoading, error: movieError } = useLoadTrending("movie");
     const { details: tvDetails, loading: tvLoading, error: tvError } = useLoadTrending("tv");
-    
+
     return (
-        <>
-            <h1>Recommended <span className="highlight">MOVIE</span> for today</h1>
+        <div className="homeContainer">
+            <h1>Recommended <span className="highlight">MOVIE</span> for today:</h1>
             {movieDetails && <Slider details={movieDetails} isReverse={false} />}
 
-            <h1>Recommended <span className="highlight">SERIES</span> for today</h1>
+            <h1>Recommended <span className="highlight">SERIES</span> for today:</h1>
             {tvDetails && <Slider details={tvDetails} isReverse={true} />}
-        </>
+
+        </div>
     );
 }
