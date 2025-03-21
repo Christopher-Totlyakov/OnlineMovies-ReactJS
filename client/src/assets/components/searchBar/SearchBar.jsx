b90127fc3573c637735f725b8c0a08fa82320cd1
Christopher Totlyakov <laminex0622@gmail.com>
2025-03-21 12:46:20 +0200

    add radioButton in searchBar

--- client/src/assets/components/searchBar/SearchBar.jsx
+++ client/src/assets/components/searchBar/SearchBar.jsx
@@ -4,22 +4,21 @@ import styles from './components/SearchBar.module.css';
 import MultiRangeSlider from "./components/MultiRangeSlider";
 import TextBox from "./components/TextBox";
 
-
 export function SearchBar({ onSearch }) {
-    const [query, setQuery] = useState
-        ({
-            type: "movie",
-            name: '',
-            prYear: '',
-            gteYear: '',
-            lteYear: '',
-            page: 1,
-            gteVote: '',
-            lteVote: '',
-        });
+    const [query, setQuery] = useState({
+        type: "movie",
+        name: '',
+        prYear: '',
+        gteYear: '',
+        lteYear: '',
+        page: 1,
+        gteVote: '',
+        lteVote: '',
+    });
+
+    const [searchMode, setSearchMode] = useState("basic"); // "basic" или "advanced"
 
     const currentYear = new Date().getFullYear();
-
 
     const handleChange = useCallback((e) => {
         if (e.type === "YearRange") {
@@ -38,44 +37,81 @@ export function SearchBar({ onSearch }) {
 
     return (
         <form onSubmit={handleSubmit} className={styles['searchForm']}>
+           
+            <div className={styles["radio-group"]}>
+                
+                <label className={styles["radioContainer"]}>
+                    <input
+                        type="radio"
+                        name="searchMode"
+                        value="basic"
+                        checked={searchMode === "basic"}
+                        onChange={() => setSearchMode("basic")}
+                    />
+                    Basic Search
+                    <div className={styles["checkmark"]}></div>
+                </label>
 
-            <div>
-                <TextBox
-                    type="text"
-                    name="name"
-                    placeholder="Keyword..."
-                    value={query.name}
-                    onChange={handleChange}
-                />
-                <TextBox
-                    type="number"
-                    name="prYear"
-                    placeholder="Year"
-                    value={query.year}
-                    onChange={handleChange}
-                />
+                <label className={styles["radioContainer"]}>
+                    <input
+                        type="radio"
+                        name="searchMode"
+                        value="advanced"
+                        checked={searchMode === "advanced"}
+                        onChange={() => setSearchMode("advanced")}
+                    />
+                    Advanced Search
+                    <div className={styles["checkmark"]}></div>
+                </label>
             </div>
-            <div>
-                <label htmlFor="Years">Years</label>
-                <MultiRangeSlider
-                    min={1900}
-                    max={currentYear}
-                    name="YearRange"
-                    onChange={handleChange}
-                />
 
-                <label htmlFor="Rating">Rating</label>
-                <MultiRangeSlider
-                    min={0}
-                    max={10}
-                    name="RatingRange"
-                    onChange={handleChange}
-                />
-            </div>
-            <button
-                type="submit"
-                className={styles['searchContent']}
-            >Search</button>
+            {/* Basic Search */}
+            {searchMode === "basic" && (
+                <div className={styles["inputsConteiner"]}>
+                    <TextBox
+                        type="text"
+                        name="name"
+                        placeholder="Keyword..."
+                        value={query.name}
+                        onChange={handleChange}
+                    />
+                    <TextBox
+                        type="number"
+                        name="prYear"
+                        placeholder="Year"
+                        value={query.prYear}
+                        onChange={handleChange}
+                    />
+                </div>
+            )}
+
+            {/* Advanced Search */}
+            {searchMode === "advanced" && (
+                <div className={styles["inputsConteiner"]}>
+                    <label htmlFor="Years">
+                        <p className={styles["label"]}>Years</p>
+                        <MultiRangeSlider
+                            min={1900}
+                            max={currentYear}
+                            name="YearRange"
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <label htmlFor="Rating">
+                        <p className={styles["label"]}>Rating</p>
+                        <MultiRangeSlider
+                            min={0}
+                            max={10}
+                            name="RatingRange"
+                            onChange={handleChange}
+                        />
+                </label>
+                </div>
+            )}
+
+            <button type="submit" className={styles['searchContent']}>
+                Search
+            </button>
         </form>
     );
 }
