9bc289a448cbf7b4ad10fe75f44933d72ffd968c
Christopher Totlyakov <laminex0622@gmail.com>
2025-03-21 11:47:18 +0200

    add play element in movieElement

--- client/src/assets/components/shared/MovieElement.jsx
+++ client/src/assets/components/shared/MovieElement.jsx
@@ -5,6 +5,7 @@ export function MovieElement({type, id, img, title, year}) {
     return (
         <Link to={`/${type}/${id}`}>
             <div className="card-wrapper">
+                <div className='playImg'></div>
                 <div className="card" style={{ '--card-bg-img': `url(https://image.tmdb.org/t/p/w200${img})` }}>
                     <p className="heading">{title}</p>
                     <p>{year}</p>
