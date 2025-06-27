88081484e427bbda0cc3593a1b5985d2e6ccf0d0
Christopher Totlyakov <laminex0622@gmail.com>
2025-06-27 19:06:45 +0300

    fix Not Found image url

--- client/src/assets/components/404.jsx
+++ client/src/assets/components/404.jsx
@@ -3,6 +3,6 @@ import "./404.css"
 export default function NotFound(){
 
     return (<div className="homeContainer">
-        <img className="imgError" src="404.png" alt="Not Found image" />
+        <img className="imgError" src="/404.png" alt="Not Found image" />
     </div>)
 }
